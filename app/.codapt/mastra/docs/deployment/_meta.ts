aa2cecf87e3dafa48d8a710519f32390a08c63e7
leaps <lmtejedor@gmail.com>
2025-07-15 16:41:38 +0200

    Add mastra documentation for codapt

---
+++ app/.codapt/mastra/docs/deployment/_meta.ts
@@ -0,0 +1,9 @@
+const meta = {
+  overview: "Overview",
+  "server-deployment": "Server deployment",
+  "web-framework": "With a Web Framework",
+  "serverless-platforms": "Serverless Platforms",
+  "cloud-providers": "Cloud Providers",
+};
+
+export default meta;
